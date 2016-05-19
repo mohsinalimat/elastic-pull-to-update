f96cb16ca3ff1233972662c0e468731fe1096d59
Mikhail Stepkin <mikhail.s@ramotion.com>
2016-05-19 16:16:05 +0300

    Initial Commit

---
+++ ElasticPullToUpdate/ElasticPullToUpdate.h
@@ -0,0 +1,19 @@
+//
+//  ElasticPullToUpdate.h
+//  ElasticPullToUpdate
+//
+//  Created by Mikhail Stepkin on 19.05.16.
+//  Copyright © 2016 Ramotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElasticPullToUpdate.
+FOUNDATION_EXPORT double ElasticPullToUpdateVersionNumber;
+
+//! Project version string for ElasticPullToUpdate.
+FOUNDATION_EXPORT const unsigned char ElasticPullToUpdateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElasticPullToUpdate/PublicHeader.h>
+
+
